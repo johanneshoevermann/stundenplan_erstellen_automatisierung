8ca14db162197f22f3e9aa12fdc2c73ff75eb1f3
Johannes Hövermann <johannes.hoevermann@outlook.de>
2024-09-22 18:44:07 +0200

    Beispiel-Studenten angelegt

---
+++ studentendaten.py
@@ -0,0 +1,129 @@
+from studenten import Student
+
+student1 = Student("Max Mustermann", {
+    "Montag": [(9, 10), (14, 16)],
+    "Dienstag": [(10, 12)],
+    "Mittwoch": [(12, 14)],
+}, 60)
+
+student2 = Student("Anna Beispiel", {
+    "Montag": [(8, 9)],
+    "Mittwoch": [(13, 14)],
+    "Freitag": [(9, 10)],
+}, 45)
+
+student3 = Student("Lisa Müller", {
+    "Dienstag": [(9, 10)],
+    "Donnerstag": [(11, 13)],
+    "Freitag": [(10, 12)],
+}, 30)
+
+student4 = Student("Tom Schmidt", {
+    "Montag": [(14, 15)],
+    "Mittwoch": [(10, 11)],
+    "Donnerstag": [(9, 10)],
+}, 60)
+
+student5 = Student("Clara Weber", {
+    "Montag": [(9, 11)],
+    "Freitag": [(14, 15)],
+}, 45)
+
+student6 = Student("Ben Fischer", {
+    "Dienstag": [(8, 9), (15, 16)],
+    "Donnerstag": [(12, 14)],
+}, 60)
+
+student7 = Student("Emma Schröder", {
+    "Mittwoch": [(10, 12)],
+    "Freitag": [(11, 12)],
+}, 30)
+
+student8 = Student("Lukas Meier", {
+    "Montag": [(9, 10), (16, 17)],
+    "Dienstag": [(13, 14)],
+}, 45)
+
+student9 = Student("Sophia Neumann", {
+    "Montag": [(8, 9)],
+    "Mittwoch": [(15, 16)],
+}, 60)
+
+student10 = Student("Felix Wagner", {
+    "Dienstag": [(9, 10)],
+    "Donnerstag": [(14, 16)],
+}, 60)
+
+student11 = Student("Marie Becker", {
+    "Montag": [(14, 15)],
+    "Dienstag": [(10, 12)],
+}, 30)
+
+student12 = Student("Elias Bauer", {
+    "Donnerstag": [(8, 9)],
+    "Freitag": [(14, 15)],
+}, 45)
+
+student13 = Student("Lea Hoffmann", {
+    "Dienstag": [(11, 12)],
+    "Mittwoch": [(9, 10)],
+}, 30)
+
+student14 = Student("Jonas Peters", {
+    "Montag": [(12, 13)],
+    "Freitag": [(8, 9)],
+}, 45)
+
+student15 = Student("Mia Köhler", {
+    "Donnerstag": [(10, 11), (14, 15)],
+}, 60)
+
+student16 = Student("David Lang", {
+    "Dienstag": [(10, 11)],
+    "Mittwoch": [(12, 13)],
+}, 45)
+
+student17 = Student("Hannah Wolf", {
+    "Montag": [(14, 16)],
+    "Freitag": [(9, 11)],
+}, 60)
+
+student18 = Student("Paul Krause", {
+    "Dienstag": [(9, 10)],
+    "Donnerstag": [(13, 14)],
+}, 45)
+
+student19 = Student("Julia Lehmann", {
+    "Montag": [(11, 12)],
+    "Mittwoch": [(14, 15)],
+}, 30)
+
+student20 = Student("Lena Schwarz", {
+    "Donnerstag": [(12, 14)],
+    "Freitag": [(10, 11)],
+}, 60)
+
+student21 = Student("Tim Krüger", {
+    "Montag": [(9, 10)],
+    "Mittwoch": [(15, 16)],
+}, 45)
+
+student22 = Student("Nina Braun", {
+    "Dienstag": [(14, 15)],
+    "Donnerstag": [(9, 10)],
+}, 30)
+
+student23 = Student("Jan Richter", {
+    "Mittwoch": [(8, 9), (14, 15)],
+    "Freitag": [(11, 12)],
+}, 60)
+
+student24 = Student("Laura Busch", {
+    "Dienstag": [(10, 12)],
+    "Donnerstag": [(15, 16)],
+}, 45)
+
+student25 = Student("Sarah Keller", {
+    "Montag": [(14, 16)],
+    "Freitag": [(8, 10)],
+}, 60)
