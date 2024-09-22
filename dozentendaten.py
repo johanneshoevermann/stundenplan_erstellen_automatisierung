f9baf3f206578996e09b8b9ace2a45c1ed92cf2d
Johannes Hövermann <johannes.hoevermann@outlook.de>
2024-09-22 18:45:06 +0200

    dozentendaten update

--- dozentendaten.py
+++ dozentendaten.py
@@ -1,8 +1,21 @@
 from dozenten import Dozent
 
-dozent1 = Dozent("Markus Müller", {"Montag": [(9, 16)], "Dienstag": [
-                 (9, 16)], "Mittwoch": [(9, 16)], "Donnerstag": [(9, 16)], "Freitag": [(9, 16)]})
-dozent2 = Dozent("Nina Niemeyer", {"Montag": [(11, 18)], "Dienstag": [
-                 (11, 18)], "Mittwoch": [(11, 18)], "Donnerstag": [(11, 18)], "Freitag": [(12, 15)]})
-dozent3 = Dozent("Franz Fröhlich", {"Montag": [(8, 12)], "Dienstag": [
-                 (9, 16)], "Mittwoch": [(14, 20)], "Donnerstag": [(9, 20)], "Freitag": [(9, 12)]})
+dozent1 = Dozent("Herr Müller", {
+    "Montag": [(9, 13)],
+    "Dienstag": [(10, 14)],
+    "Donnerstag": [(9, 12)]
+})
+
+dozent2 = Dozent("Frau Schmidt", {
+    "Montag": [(13, 16)],
+    "Mittwoch": [(9, 12)],
+    "Freitag": [(10, 13)]
+})
+
+dozent3 = Dozent("Herr Becker", {
+    "Dienstag": [(8, 12)],
+    "Donnerstag": [(11, 15)],
+    "Freitag": [(9, 14)]
+})
+
+dozenten_liste = [dozent1, dozent2, dozent3]
