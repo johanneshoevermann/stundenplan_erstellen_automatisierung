fdd9ba8ab5fe7733797d4a49810a12943afdd11d
Johannes Hövermann <johannes.hoevermann@outlook.de>
2024-09-22 18:34:19 +0200

    Beispiel-Dozenten angelegt

---
+++ dozentendaten.py
@@ -0,0 +1,8 @@
+from dozenten import Dozent
+
+dozent1 = Dozent("Markus Müller", {"Montag": [(9, 16)], "Dienstag": [
+                 (9, 16)], "Mittwoch": [(9, 16)], "Donnerstag": [(9, 16)], "Freitag": [(9, 16)]})
+dozent2 = Dozent("Nina Niemeyer", {"Montag": [(11, 18)], "Dienstag": [
+                 (11, 18)], "Mittwoch": [(11, 18)], "Donnerstag": [(11, 18)], "Freitag": [(12, 15)]})
+dozent3 = Dozent("Franz Fröhlich", {"Montag": [(8, 12)], "Dienstag": [
+                 (9, 16)], "Mittwoch": [(14, 20)], "Donnerstag": [(9, 20)], "Freitag": [(9, 12)]})
