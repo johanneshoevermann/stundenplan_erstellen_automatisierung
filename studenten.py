2a5b1bc2a29fe518ea1771eda926c41e5718aab0
Johannes Hövermann <johannes.hoevermann@outlook.de>
2024-09-23 09:04:49 +0200

    Schreibfehler korrigiert

--- studenten.py
+++ studenten.py
@@ -1,7 +1,7 @@
 class Student:
     def __init__(self, name, verfuegbare_zeiten, unterrichtsdauer):
         self.name = name
-        self.verfügbare_zeiten = verfuegbare_zeiten
+        self.verfuegbare_zeiten = verfuegbare_zeiten
         self.unterrichtsdauer = unterrichtsdauer
 
     # Methode, um eine gut lesbare Repräsentation eines Objektes der Klasse ausgeben zu können. Dient mir, dem Entwickler, um einfach sehen zu können, welche Daten in einem Objekt enthalten sind.
