d434b138f7bdfaef039c0eea54432acf2dadb1ea
Johannes Hövermann <johannes.hoevermann@outlook.de>
2024-09-23 08:36:48 +0200

    Verfügbarkeiten in sinnvolleres Zeit-Format geändert

--- dozentendaten.py
+++ dozentendaten.py
@@ -1,21 +1,24 @@
+from datetime import time
 from dozenten import Dozent
 
+# Liste der Dozierenden mit genauen Zeitfenstern (Stunden und Minuten)
 dozent1 = Dozent("Herr Müller", {
-    "Montag": [(9, 13)],
-    "Dienstag": [(10, 14)],
-    "Donnerstag": [(9, 12)]
+    "Montag": [(time(9, 0), time(13, 0))],
+    "Dienstag": [(time(10, 0), time(14, 0))],
+    "Donnerstag": [(time(9, 0), time(12, 0))]
 })
 
 dozent2 = Dozent("Frau Schmidt", {
-    "Montag": [(13, 16)],
-    "Mittwoch": [(9, 12)],
-    "Freitag": [(10, 13)]
+    "Montag": [(time(13, 0), time(16, 0))],
+    "Mittwoch": [(time(9, 0), time(12, 0))],
+    "Freitag": [(time(10, 0), time(13, 0))]
 })
 
 dozent3 = Dozent("Herr Becker", {
-    "Dienstag": [(8, 12)],
-    "Donnerstag": [(11, 15)],
-    "Freitag": [(9, 14)]
+    "Dienstag": [(time(8, 0), time(12, 0))],
+    "Donnerstag": [(time(11, 0), time(15, 0))],
+    "Freitag": [(time(9, 0), time(14, 0))]
 })
 
+# Alle Dozierenden in eine Liste packen
 dozenten_liste = [dozent1, dozent2, dozent3]
