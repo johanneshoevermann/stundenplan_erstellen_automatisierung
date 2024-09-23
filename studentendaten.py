b84ac300315fecf262a2c2ccefb0dc62dacf6c90
Johannes Hövermann <johannes.hoevermann@outlook.de>
2024-09-23 08:35:09 +0200

    Verfügbarkeiten in sinnvolleres Zeit-Format geändert

--- studentendaten.py
+++ studentendaten.py
@@ -1,129 +1,139 @@
+from datetime import time
 from studenten import Student
 
+# Liste der Studierenden mit genauen Zeitfenstern (Stunden und Minuten)
 student1 = Student("Max Mustermann", {
-    "Montag": [(9, 10), (14, 16)],
-    "Dienstag": [(10, 12)],
-    "Mittwoch": [(12, 14)],
+    "Montag": [(time(9, 0), time(10, 15)), (time(14, 0), time(15, 45))],
+    "Dienstag": [(time(10, 15), time(12, 0))],
+    "Mittwoch": [(time(12, 0), time(14, 0))]
 }, 60)
 
 student2 = Student("Anna Beispiel", {
-    "Montag": [(8, 9)],
-    "Mittwoch": [(13, 14)],
-    "Freitag": [(9, 10)],
+    "Montag": [(time(8, 0), time(9, 0))],
+    "Mittwoch": [(time(13, 0), time(14, 0))],
+    "Freitag": [(time(9, 0), time(10, 0))]
 }, 45)
 
 student3 = Student("Lisa Müller", {
-    "Dienstag": [(9, 10)],
-    "Donnerstag": [(11, 13)],
-    "Freitag": [(10, 12)],
+    "Dienstag": [(time(9, 0), time(10, 0))],
+    "Donnerstag": [(time(11, 0), time(13, 0))],
+    "Freitag": [(time(10, 0), time(12, 0))]
 }, 30)
 
 student4 = Student("Tom Schmidt", {
-    "Montag": [(14, 15)],
-    "Mittwoch": [(10, 11)],
-    "Donnerstag": [(9, 10)],
+    "Montag": [(time(14, 0), time(15, 0))],
+    "Mittwoch": [(time(10, 0), time(11, 0))],
+    "Donnerstag": [(time(9, 0), time(10, 0))]
 }, 60)
 
 student5 = Student("Clara Weber", {
-    "Montag": [(9, 11)],
-    "Freitag": [(14, 15)],
+    "Montag": [(time(9, 0), time(11, 0))],
+    "Freitag": [(time(14, 0), time(15, 0))]
 }, 45)
 
 student6 = Student("Ben Fischer", {
-    "Dienstag": [(8, 9), (15, 16)],
-    "Donnerstag": [(12, 14)],
+    "Dienstag": [(time(8, 0), time(9, 0)), (time(15, 0), time(16, 0))],
+    "Donnerstag": [(time(12, 0), time(14, 0))]
 }, 60)
 
 student7 = Student("Emma Schröder", {
-    "Mittwoch": [(10, 12)],
-    "Freitag": [(11, 12)],
+    "Mittwoch": [(time(10, 0), time(12, 0))],
+    "Freitag": [(time(11, 0), time(12, 0))]
 }, 30)
 
 student8 = Student("Lukas Meier", {
-    "Montag": [(9, 10), (16, 17)],
-    "Dienstag": [(13, 14)],
+    "Montag": [(time(9, 0), time(10, 0)), (time(16, 0), time(17, 0))],
+    "Dienstag": [(time(13, 0), time(14, 0))]
 }, 45)
 
 student9 = Student("Sophia Neumann", {
-    "Montag": [(8, 9)],
-    "Mittwoch": [(15, 16)],
+    "Montag": [(time(8, 0), time(9, 0))],
+    "Mittwoch": [(time(15, 0), time(16, 0))]
 }, 60)
 
 student10 = Student("Felix Wagner", {
-    "Dienstag": [(9, 10)],
-    "Donnerstag": [(14, 16)],
+    "Dienstag": [(time(9, 0), time(10, 0))],
+    "Donnerstag": [(time(14, 0), time(16, 0))]
 }, 60)
 
 student11 = Student("Marie Becker", {
-    "Montag": [(14, 15)],
-    "Dienstag": [(10, 12)],
+    "Montag": [(time(14, 0), time(15, 0))],
+    "Dienstag": [(time(10, 0), time(12, 0))]
 }, 30)
 
 student12 = Student("Elias Bauer", {
-    "Donnerstag": [(8, 9)],
-    "Freitag": [(14, 15)],
+    "Donnerstag": [(time(8, 0), time(9, 0))],
+    "Freitag": [(time(14, 0), time(15, 0))]
 }, 45)
 
 student13 = Student("Lea Hoffmann", {
-    "Dienstag": [(11, 12)],
-    "Mittwoch": [(9, 10)],
+    "Dienstag": [(time(11, 0), time(12, 0))],
+    "Mittwoch": [(time(9, 0), time(10, 0))]
 }, 30)
 
 student14 = Student("Jonas Peters", {
-    "Montag": [(12, 13)],
-    "Freitag": [(8, 9)],
+    "Montag": [(time(12, 0), time(13, 0))],
+    "Freitag": [(time(8, 0), time(9, 0))]
 }, 45)
 
 student15 = Student("Mia Köhler", {
-    "Donnerstag": [(10, 11), (14, 15)],
+    "Donnerstag": [(time(10, 0), time(11, 0)), (time(14, 0), time(15, 0))]
 }, 60)
 
 student16 = Student("David Lang", {
-    "Dienstag": [(10, 11)],
-    "Mittwoch": [(12, 13)],
+    "Dienstag": [(time(10, 0), time(11, 0))],
+    "Mittwoch": [(time(12, 0), time(13, 0))]
 }, 45)
 
 student17 = Student("Hannah Wolf", {
-    "Montag": [(14, 16)],
-    "Freitag": [(9, 11)],
+    "Montag": [(time(14, 0), time(16, 0))],
+    "Freitag": [(time(9, 0), time(11, 0))]
 }, 60)
 
 student18 = Student("Paul Krause", {
-    "Dienstag": [(9, 10)],
-    "Donnerstag": [(13, 14)],
+    "Dienstag": [(time(9, 0), time(10, 0))],
+    "Donnerstag": [(time(13, 0), time(14, 0))]
 }, 45)
 
 student19 = Student("Julia Lehmann", {
-    "Montag": [(11, 12)],
-    "Mittwoch": [(14, 15)],
+    "Montag": [(time(11, 0), time(12, 0))],
+    "Mittwoch": [(time(14, 0), time(15, 0))]
 }, 30)
 
 student20 = Student("Lena Schwarz", {
-    "Donnerstag": [(12, 14)],
-    "Freitag": [(10, 11)],
+    "Donnerstag": [(time(12, 0), time(14, 0))],
+    "Freitag": [(time(10, 0), time(11, 0))]
 }, 60)
 
 student21 = Student("Tim Krüger", {
-    "Montag": [(9, 10)],
-    "Mittwoch": [(15, 16)],
+    "Montag": [(time(9, 0), time(10, 0))],
+    "Mittwoch": [(time(15, 0), time(16, 0))]
 }, 45)
 
 student22 = Student("Nina Braun", {
-    "Dienstag": [(14, 15)],
-    "Donnerstag": [(9, 10)],
+    "Dienstag": [(time(14, 0), time(15, 0))],
+    "Donnerstag": [(time(9, 0), time(10, 0))]
 }, 30)
 
 student23 = Student("Jan Richter", {
-    "Mittwoch": [(8, 9), (14, 15)],
-    "Freitag": [(11, 12)],
+    "Mittwoch": [(time(8, 0), time(9, 0)), (time(14, 0), time(15, 0))],
+    "Freitag": [(time(11, 0), time(12, 0))]
 }, 60)
 
 student24 = Student("Laura Busch", {
-    "Dienstag": [(10, 12)],
-    "Donnerstag": [(15, 16)],
+    "Dienstag": [(time(10, 0), time(12, 0))],
+    "Donnerstag": [(time(15, 0), time(16, 0))]
 }, 45)
 
 student25 = Student("Sarah Keller", {
-    "Montag": [(14, 16)],
-    "Freitag": [(8, 10)],
+    "Montag": [(time(14, 0), time(16, 0))],
+    "Freitag": [(time(8, 0), time(10, 0))]
 }, 60)
+
+# Alle Studierenden in eine Liste packen
+studenten_liste = [
+    student1, student2, student3, student4, student5, student6, student7,
+    student8, student9, student10, student11, student12, student13, student14,
+    student15, student16, student17, student18, student19, student20, student21,
+    student22, student23, student24, student25
+]
