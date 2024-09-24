c8781baa5f1bc89e20e385533b048c9b407b3c81
Johannes Hövermann <johannes.hoevermann@outlook.de>
2024-09-24 09:42:17 +0200

    Created Functions: convert_time_to_datetime, get_intervals

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from studentendaten import *
 from dozentendaten import *
+import datetime
 
 
 def wer_hat_wann_zeit():
@@ -18,4 +19,71 @@ def wer_hat_wann_zeit():
         print("\n\n\n")
 
 
-wer_hat_wann_zeit()
+# wer_hat_wann_zeit()
+
+# for intervall in dozent1.verfuegbare_zeiten["Montag"]:
+    print(intervall)
+
+
+# tstart = datetime.time(9, 0)
+# tend = datetime.time(13, 0)
+# interval = datetime.timedelta(minutes=30)
+
+# periods = []
+
+# period_start = tstart
+# while period_start < tend:
+#     print(period_start)
+#     newTime = period_start + interval
+#     period_end = min(period_start + interval, tend)
+#     periods.append((period_start, period_end))
+#     period_start = period_end
+
+# print(periods)
+
+# date_and_time = datetime.datetime(2021, 8)
+# print("Original time:")
+# print(date_and_time)
+
+# # Calling the timedelta() function
+# time_change = datetime.timedelta(minutes=75)
+# new_time = date_and_time + time_change
+
+# # Printing the new datetime object
+# print("changed time:")
+# print(new_time)
+
+tstart = datetime.time(9, 0)
+tend = datetime.time(13, 0)
+
+
+def convert_time_to_datetime(time):
+    date = datetime.date.today()
+    print(date)
+    date_time = datetime.datetime.combine(date, time)
+    return (date_time)
+
+
+# convert_time_to_datetime(tstart)
+
+def get_intervals(start, end, interval):
+    tstart = start
+    tend = end
+    tinterval = datetime.timedelta(minutes=interval)
+
+    periods = []
+
+    period_start = tstart
+    while period_start < tend:
+        period_end = min(period_start + tinterval, tend)
+        periods.append((period_start, period_end))
+        period_start = period_end
+
+    print(periods)
+
+
+new_start = convert_time_to_datetime(tstart)
+new_end = convert_time_to_datetime(tend)
+
+
+get_intervals(new_start, new_end, 30)
