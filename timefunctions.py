713afa13eafb641097b566eeca864f38445e9dcd
Johannes Hövermann <johannes.hoevermann@outlook.de>
2024-09-24 10:51:14 +0200

    added get_intervals functions

--- timefunctions.py
+++ timefunctions.py
@@ -1,8 +1,24 @@
 import datetime
 
+periods = []
+
 
 def convert_time_to_datetime(time):
     date = datetime.date.today()
     print(date)
     date_time = datetime.datetime.combine(date, time)
     return (date_time)
+
+
+def get_intervals(start, end, interval):
+    tstart = start
+    tend = end
+    tinterval = datetime.timedelta(minutes=interval)
+
+    period_start = tstart
+    while period_start < tend:
+        period_end = min(period_start + tinterval, tend)
+        periods.append((period_start, period_end))
+        period_start = period_end
+
+    return periods
