8357bb816b347065ec733f7dbb57460b90fa116e
Johannes Hövermann <johannes.hoevermann@outlook.de>
2024-09-24 11:05:28 +0200

    removed print statement in cenvert function

--- timefunctions.py
+++ timefunctions.py
@@ -5,7 +5,6 @@ periods = []
 
 def convert_time_to_datetime(time):
     date = datetime.date.today()
-    print(date)
     date_time = datetime.datetime.combine(date, time)
     return (date_time)
 
