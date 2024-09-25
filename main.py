038148db1f511ded8e2c429519011edb3bff8189
Johannes Hövermann <johannes.hoevermann@outlook.de>
2024-09-25 18:56:41 +0200

    created function get_zeittupel

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
-import datetime
+import pprint
 from studentendaten import *
 from dozentendaten import *
 from timefunctions import convert_time_to_datetime, get_intervals
-from dozentenintervalle import Vierergruppen
+from intervalle import *
 
-# bei mehreren Zeitfenstern gibt es hier wohl ein problem da die Varaiablen überschireben werden.
-for entry in dozent1.verfuegbare_zeiten["Montag"]:
-    anfangszeit, endzeit = entry
 
-tstart = convert_time_to_datetime(anfangszeit)
-tend = convert_time_to_datetime(endzeit)
+def get_zeittupel(person, tag):
+    for entry in person.verfuegbare_zeiten[tag]:
+        anfangszeit, endzeit = entry
+        tstart = convert_time_to_datetime(anfangszeit)
+        tend = convert_time_to_datetime(endzeit)
 
-periods = get_intervals(tstart, tend, 15)
+        periods = get_intervals(tstart, tend, 15)
 
-gruppen = Vierergruppen(periods)
+        if isinstance(person, Student):
+            if person.unterrichtsdauer == 30:
+                return Zweiergruppen(periods)
+            elif person.unterrichtsdauer == 45:
+                return Dreiergruppen(periods)
+            elif person.unterrichtsdauer == 60:
+                return Vierergruppen(periods)
+            else:
+                return "Fehler: keine sinvolle Unterrichtslänge vom Studenten angegeben."
 
-for gruppe in gruppen:
-    print(f"\n{gruppe}")
+        if isinstance(person, Dozent):
+            return Zweiergruppen(periods), Dreiergruppen(periods), Vierergruppen(periods)
+
+
+pprint.pprint(get_zeittupel(dozent2, "Montag"))
