3e3ced390650eeb201b63abd90e5bc194e25621b
Johannes Hövermann <johannes.hoevermann@outlook.de>
2024-09-25 16:54:25 +0200

    Added Dreier- und Zweiergruppen

--- dozentenintervalle.py
+++ dozentenintervalle.py
@@ -8,3 +8,21 @@ def Vierergruppen(list):
         gruppen.append(list[i:i+4])
 
     return gruppen
+
+
+def Dreiergruppen(list):
+    gruppen = []
+
+    for i in range(0, len(list), 1):
+        gruppen.append(list[i:i+3])
+
+    return gruppen
+
+
+def Zweiergruppen(list):
+    gruppen = []
+
+    for i in range(0, len(list), 1):
+        gruppen.append(list[i:i+3])
+
+    return gruppen
