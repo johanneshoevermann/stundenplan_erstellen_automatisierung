e17c1f74c05282d366c3ba89727851f06a28e346
Johannes Hövermann <johannes.hoevermann@outlook.de>
2024-09-27 10:16:10 +0200

    remove dozenten.py

--- dozenten.py
+++
@@ -1,7 +0,0 @@
-class Dozent:
-    def __init__(self, name, verfuegbare_zeiten):
-        self.name = name
-        self.verfuegbare_zeiten = verfuegbare_zeiten
-
-    def __repr__(self):
-        return f"Dozent({self.name}, {self.verfuegbare_zeiten})"
