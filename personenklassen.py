c1cc9648d5fb4a05e2856539fd22bd2412e25d2b
Johannes Hövermann <johannes.hoevermann@outlook.de>
2024-09-27 11:02:35 +0200

    removed old class version from the top

--- personenklassen.py
+++ personenklassen.py
@@ -1,21 +1,3 @@
-""" class Person:
-    def __init__(self, name, verfuegbare_zeiten):
-        self.name = name
-        self.verfuegbare_zeiten = verfuegbare_zeiten
-
-
-class Student(Person):
-    def __init__(self, name, verfuegbare_zeiten, unterrichtsdauer):
-        super().__init__(name, verfuegbare_zeiten)
-        self.unterrichtsdauer = unterrichtsdauer
-        
-
-
-class Dozent(Person):
-    pass
-  """
-
-
 import datetime
 
 
